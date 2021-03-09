61d653aace3a19aa07650d41c16a9cc1341ab693
DucTrongVo <duc-trong.vo@toulouse.miage.fr>
2021-03-09 15:30:25 +0100

    first commit

---
+++ src/main/java/m2/miage/m2gestioncours/M2GestionCoursApplication.java
@@ -0,0 +1,13 @@
+package m2.miage.m2gestioncours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2GestionCoursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M2GestionCoursApplication.class, args);
+    }
+
+}
---
+++ src/test/java/m2/miage/m2gestioncours/M2GestionCoursApplicationTests.java
@@ -0,0 +1,13 @@
+package m2.miage.m2gestioncours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2GestionCoursApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
